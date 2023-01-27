1ee7e12c388a5a6699aea082ec12c092d9364ca8
adam willetts <adam-willetts@hotmail.co.uk>
2023-01-27 19:58:36 +0000

    created a var to store the current time

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,9 @@
 
 // global variables 
 let currentDayEl = $("#currentDay");
-
-
+let currentTime = moment().format("LT");
+let currentHour =$(".hour");
 // set todays date.
 currentDayEl.text(moment().format('DD-MM-YY'));
+
+// time conditions 
