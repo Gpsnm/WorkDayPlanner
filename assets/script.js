3818e2ff81a6ac9783b0da194ed526fe4173acee
adam willetts <adam-willetts@hotmail.co.uk>
2023-01-28 18:09:11 +0000

    added font size to textarea in css

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,53 @@
-
 // global variables 
 let testTime = "13:00";
 let currentDayEl = $("#currentDay");
-let currentTime = moment().format("LT");
+let currentTime = parseInt(moment().format("LT"));
 let rowEl = $(".row");
-let hourArr = Array.from(rowEl)
-
+let hour1 =$("");
 // set todays date.
 currentDayEl.text(moment().format('DD-MM-YY'));
+// 
+// 
+// 
+// 
+// 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // time conditions
 
-console.log(hourArr);
+// console.log(currentTime);
+// // break it down code.
+
+// let timeId =(parseInt($(".hour").html()));
+
+// if (timeId < currentTime) {
+//     rowEl.addClass("past");
+    
+// } else if (timeId >= currentTime){
+//     rowEl.addClass("present");
+// }
 
 
+// console.log(timeId);
