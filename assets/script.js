dde8ad81108b9fe63b5ea49adbc027731ed57e6f
adam willetts <adam-willetts@hotmail.co.uk>
2023-01-28 09:48:16 +0000

    added a var that adds all of my rowel into an array

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,16 @@
 
 // global variables 
+let testTime = "13:00";
 let currentDayEl = $("#currentDay");
 let currentTime = moment().format("LT");
-let currentHour =$(".hour");
+let rowEl = $(".row");
+let hourArr = Array.from(rowEl)
+
 // set todays date.
 currentDayEl.text(moment().format('DD-MM-YY'));
 
-// time conditions 
+// time conditions
+
+console.log(hourArr);
+
+
