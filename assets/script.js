1dac8fc948242041f1d784c06dd5244cffe129ad
adam willetts <adam-willetts@hotmail.co.uk>
2023-01-28 20:35:59 +0000

    replaced data attr with id found a way to make js work

--- assets/script.js
+++ assets/script.js
@@ -4,12 +4,6 @@ let currentDayEl = $("#currentDay");
 let currentTime = moment().format("HH");
 let rowEl = $(".row");
 let hourEl = $(".hour");
-let textArea = $(".time-block");
-let hour1 = $("#hour1")
-let hourNeeded;
+
 // set todays date.
 currentDayEl.text(moment().format("DD-MM-YY"));
-
-let timeNeed = rowEl.attr('data-time');
-
-console.log(timeNeed);
