c9ce4dc62666852c47864f24c9495b9f2342e0f9
adam willetts <adam-willetts@hotmail.co.uk>
2023-01-29 17:19:23 +0000

    changed classes on html to not clash with js css selectors

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,7 @@
 let currentDayEl = $("#currentDay");
 let currentTime = moment().format("HH");
 let timeBlock = $(".time-block");
-let saveBtn = $("#savebtn");
+let saveBtn = $(".saveButton");
 // set todays date.
 currentDayEl.text(moment().format("DD-MM-YY"));
 
@@ -15,4 +15,15 @@ timeBlock.each(function () {
     }else{
         $(this).addClass('present');
     }
+// let childTextArea = $(this).find("textarea");
+// childTextArea.val(localStorage.getItem(timeSlot));
+
 });
+
+// saveBtn.click(function (){
+//     let clickedBtn = $(this);
+//     let closestTimeBlockEl = clickedBtn.closest(".time-block");
+//     let textArea = closestTimeBlockEl.find("textarea");
+//     let textAreaVal = textArea.val();
+// localStorage.setItem(closestTimeBlockEl.attr("id"), textAreaVal);
+// })
